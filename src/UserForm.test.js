303b406d4d7d111db7e26547531866bd08aa6af3
changgun-park <pcg0537@gmail.com>
2023-03-16 02:11:07 +0900

    feat: simple test for UserForm

---
+++ src/UserForm.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import UserForm from "./UserForm";
+
+test("it shows two inputs and a button", () => {
+  // 1. render the component
+  render(<UserForm />);
+
+  // Manipulate the component or find an element in it
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  // Assertion: make sure the component is doing what we expect to do
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
