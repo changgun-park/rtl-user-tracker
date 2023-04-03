9a5f45c2e7097e3c0601e5a585c645c326b54077
changgun-park <pcg0537@gmail.com>
2023-04-04 00:11:09 +0900

    feat: add test for submit

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -14,3 +14,30 @@ test("it shows two inputs and a button", () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test("it calls onUserAdd when the form is submitted", () => {
+  const mock = jest.fn();
+  // Try to render my component
+  render(<UserForm onUserAdd={mock} />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  // Simulate typing in a name
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  // Simulate typing in an email
+  user.click(emailInput);
+  user.keyboard("jane@gmail.com");
+
+  // find the button
+  const button = screen.getByRole("button");
+
+  // simulate clicking the button
+  user.click(button);
+
+  // Assertion
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@gmail.com" });
+});
